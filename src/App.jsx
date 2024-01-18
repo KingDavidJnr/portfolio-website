b7463e68fa38de1ed23734219d789030b363c179
David Oduse <89360467+KingDavidJnr@users.noreply.github.com>
2024-01-18 01:17:38 +0000

    feat: update social media props

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "David Oduse",
+  title: "Software Enigineer (Backend)",
+  email: "odusedavid@gmail.com",
+  gitHub: "KingDavidJnr",
+  instagram: "King_david_jnrr",
+  linkedIn: "david-oduse",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Oduse_david",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
